46191952bf15f1991c0c2daccf4fdb8e052c1b7f
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-06 23:30:09 +0530

    add controllers and routes structure

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const User = require("../models/User");
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ success: true, data: user });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Duplicate field value`" });
+    }
+    res.status(500).json({ success: false, error: err });
+  }
+};
+
+module.exports = { createUser };
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const User = require("./models/User");
 const app = express();
 
 //configure env file
@@ -13,25 +12,15 @@ connectDB();
 //middlewares
 app.use(express.json());
 
+// routes
+const users = require("./routes/users");
+
 //first route
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.post("/api/v1/user", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ success: true, data: user });
-  } catch (err) {
-    console.log(err);
-    if (err.code === 11000) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Duplicate field value`" });
-    }
-    res.status(500).json({ success: false, error: err });
-  }
-});
+app.use("/api/v1/user", users);
 
 // set port no.
 const PORT = process.env.PORT || 6000;
